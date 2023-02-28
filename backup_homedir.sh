f6186d6b32586321d64e7285f0de4de9997f02b5
Point Break <120101283+pointbreaklogin@users.noreply.github.com>
2023-02-28 07:55:33 +0530

    Update backup_homedir.sh

--- backup_homedir.sh
+++ backup_homedir.sh
@@ -1,22 +1,33 @@
 #!/bin/bash
 
+#ip of system
+host=$(hostname -I)
+cuser=$(logname)
+
 # Set the backup directory
-backup_dir=/backups
-
-
+backup_dir=/home/$cuser/backupdir_$host
 # Set the date format for the backup files
 date_format=$(date +"%d-%m-%Y")
-
 # Set the name of the backup file
-backup_file=$user_home_$date_format.tar.gz
+backup_file=home_$cuser$date_format.tar.gz
 
 # Create the backup directory if it doesn't exist
 if [ ! -d $backup_dir ]; then
   mkdir $backup_dir
 fi
 
-# Create the backup
-tar -czvf $backup_dir/$backup_file ~/
+#changing ownership of backup dir
+#chown $cuser:$cuser $backup_dir
 
-#uploading to remote server (ssh key-based login enabled)
-rsync pointbreak@172.16.50.50:/backup /backup
+#to test creation
+#touch $backup_dir/hai1
+#touch $backup_dir/hai2
+#touch $backup_dir/hai3
+#touch $backup_dir/hai4
+
+#test permission
+#chown $cuser:$cuser $backup_dir/*
+
+
+#Create the backup
+tar -czvf /home/$cuser/$backup_dir/$backup_file /home/$cuser/*
