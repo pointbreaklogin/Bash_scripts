9d26242f1db680b352abc65630bbbce47d75319b
Point Break <120101283+pointbreaklogin@users.noreply.github.com>
2023-02-28 17:20:28 +0530

    Update backup_homedir.sh

--- backup_homedir.sh
+++ backup_homedir.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #ip of system
-host=$(hostname -I)
+host=$(ip route get 8.8.8.8 | awk -F"src " 'NR==1{split($2,a," ");print a[1]}')
 cuser=$(logname)
 
 # Set the backup directory
@@ -9,12 +9,15 @@ backup_dir=/home/$cuser/backupdir_$host
 # Set the date format for the backup files
 date_format=$(date +"%d-%m-%Y")
 # Set the name of the backup file
-backup_file=home_$cuser$date_format.tar.gz
+backup_file=home_$cuser-$date_format.tar.gz
 
 # Create the backup directory if it doesn't exist
 if [ ! -d $backup_dir ]; then
   mkdir $backup_dir
+  echo "Directory created for backup"
 fi
 
-#Create the backup
-tar -czvf /home/$cuser/$backup_dir/$backup_file /home/$cuser/*
+#Create the backup using tar command
+tar -czvf $backup_dir/$backup_file /home/$cuser/*
+echo "" 
+echo "Backup created in $backup_dir"
